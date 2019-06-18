dbbe67fdfbb9158864c8cb6d9186790106c10b66
habernet <phaberern@gmail.com>
2019-06-18 14:35:43 -0400

    Changed connection to account for Heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,23 @@ let mysql = require('mysql');
 
 
 // Create the configuration parameters
-// Might change when hosting this on heroku!
-let connection = mysql.createConnection({
-    host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: 'mmeknf8wkm51mevf',
-    password: '774c7f6e',
-    database: 'k807ms7rpuyu2tt2'
-})
+
+// If the DB is on Heroku, use the URL to connect, otherwise it's on local host.
+
+let connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'SQLmeajob1',
+        database: 'burgers_db'
+    });
+};
+
 
 //Make the connection
 connection.connect((err) => {
