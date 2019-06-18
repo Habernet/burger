2b096ffcaff0c169290e8a6c8ac04fbabbd3b92b
habernet <phaberern@gmail.com>
2019-06-18 12:18:58 -0400

    Server.js written, not tested

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 let express = require('express');
 
 let app = express();
+let PORT = process.env.PORT || 8080;
 
-//middleware
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Allow for serving of static files: CSS, JS, etc
+app.use(express.static('./public'));
 
 //routes
+require('./controllers/burgers_controllers')(app);
+
+//Kick off our server
+app.listen(PORT, () => {
+    console.log('App listening on port: ' + PORT);
+});
 
